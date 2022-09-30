e9847ed5c0a8f30bd121cbec8ead75e05727da35
jackalcooper <jackalcooper@gmail.com>
2022-09-30 10:38:25 +0800

    add

---
+++ pipes/of-t2i-no-auto-cast.py
@@ -0,0 +1,21 @@
+# CUDA_VISIBLE_DEVICES=1
+# make sure you're logged in with `huggingface-cli login`
+import oneflow as torch
+import torch as og_torch
+from diffusers import OneFlowStableDiffusionPipeline
+
+pipe = OneFlowStableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4",
+    use_auth_token=True,
+    # revision="fp16",
+    # torch_dtype=torch.float16,
+)
+
+pipe = pipe.to("cuda")
+
+prompt = "apple made a toaster that makes toast shaped like an apple"
+with og_torch.autocast("cuda"):
+    # with torch.autocast("cuda"):
+    # images = pipe(prompt).images
+    for i, image in enumerate(images):
+        image.save(f"{prompt}-{i}.png")
