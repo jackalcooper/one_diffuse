835933e7e79dc7a6c41e1b5e9f3402a14ebef2a7
jackalcooper <jackalcooper@gmail.com>
2022-11-08 23:42:46 +0800

    add

--- pipes/oneflow-t2i.py
+++ pipes/oneflow-t2i.py
@@ -21,10 +21,13 @@ args = parse_args()
 import os
 output_dir = "oneflow-sd-output"
 os.makedirs(output_dir, exist_ok=True)
+from timeit import default_timer as timer
 with torch.autocast("cuda"):
     for j in range(10):
         prompt = args.prompt
+        start = timer()
         images = pipe(prompt, compile_unet=True).images
+        print("[oneflow]", "[pipe]", f"{timer() - start}")
         for i, image in enumerate(images):
             dst = os.path.join(output_dir, f"{prompt[:100]}-{j}-{i}.png")
             image.save(dst)
