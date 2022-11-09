1f9d6d7eabcb9f9b73399adeb831de33660a2f65
jackalcooper <jackalcooper@gmail.com>
2022-11-09 08:41:24 +0800

    add

--- pipes/oneflow-t2i.py
+++ pipes/oneflow-t2i.py
@@ -23,11 +23,20 @@ output_dir = "oneflow-sd-output"
 os.makedirs(output_dir, exist_ok=True)
 from timeit import default_timer as timer
 with torch.autocast("cuda"):
-    for j in range(10):
+    for j in range(100):
         prompt = args.prompt
+        prompt = """
+        But when the melancholy fit shall fall
+Sudden from heaven like a weeping cloud,
+That fosters the droop-headed flowers all,
+And hides the green hill in an April shroud;
+Then glut thy sorrow on a morning rose,
+Or on the rainbow of the salt sand-wave,
+Or on the wealth of globed peonies …
+        """
         start = timer()
         images = pipe(prompt, compile_unet=True).images
         print("[oneflow]", "[pipe]", f"{timer() - start}")
         for i, image in enumerate(images):
-            dst = os.path.join(output_dir, f"{prompt[:100]}-{j}-{i}.png")
+            dst = os.path.join(output_dir, f"{prompt.strip()[:100]}-{j}-{i}.png")
             image.save(dst)
