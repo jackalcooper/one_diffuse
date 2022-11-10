75ef8bb5a66eadfcc5de221ce73d970e2037905f
jackalcooper <jackalcooper@gmail.com>
2022-11-10 12:02:16 +0800

    add

--- pipes/i2i.py
+++ pipes/i2i.py
@@ -6,6 +6,9 @@ from io import BytesIO
 
 from diffusers import OneFlowStableDiffusionImg2ImgPipeline as StableDiffusionImg2ImgPipeline
 
+init_image = Image.open("sketch-mountains-input.jpg").convert("RGB")
+init_image = init_image.resize((768, 512))
+
 # load the pipeline
 device = "cuda"
 model_id_or_path = "CompVis/stable-diffusion-v1-4"
@@ -16,20 +19,16 @@ pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
     use_auth_token=True
 )
 
+
 # or download via git clone https://huggingface.co/CompVis/stable-diffusion-v1-4
 # and pass `model_id_or_path="./stable-diffusion-v1-4"` without having to use `use_auth_token=True`.
 pipe = pipe.to(device)
 
-# let's download an initial image
-url = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/assets/stable-samples/img2img/sketch-mountains-input.jpg"
-
-response = requests.get(url)
-init_image = Image.open(BytesIO(response.content)).convert("RGB")
-init_image = init_image.resize((768, 512))
 
 prompt = "A fantasy landscape, trending on artstation"
 
 with autocast("cuda"):
-    images = pipe(prompt=prompt, init_image=init_image, strength=0.75, guidance_scale=7.5).images
+    images = pipe(
+        prompt=prompt, init_image=init_image, strength=0.75, guidance_scale=7.5, compile_unet=False).images
 
 images[0].save("fantasy_landscape.png")
