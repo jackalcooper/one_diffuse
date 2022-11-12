a662da5b303a0fdd5583ba539d60a7e6b7a8b0b9
jackalcooper <jackalcooper@gmail.com>
2022-11-12 23:28:42 +0800

    fix

--- pipes/oneflow-t2i.py
+++ pipes/oneflow-t2i.py
@@ -51,6 +51,7 @@ with torch.autocast("cuda"):
         height = random.choice([128, 256, 512])
         width = 512
         height = 512
+        num_inference_steps = 50
         if isinstance(pipe.scheduler, DPMSolverMultistepScheduler):
             num_inference_steps = 20
         images = pipe(
