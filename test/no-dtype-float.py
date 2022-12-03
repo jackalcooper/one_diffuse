e204ac1398611fc5247a579fb215e6237fe41c9b
jackalcooper <jackalcooper@gmail.com>
2022-12-03 14:14:26 +0800

    add

--- test/no-dtype-float.py
+++ test/no-dtype-float.py
@@ -5,13 +5,15 @@ def f(m):
     fp16_t = torch.tensor([14.6146], dtype=torch.float16)
     fp16_scalar_t = torch.tensor(14.6146, dtype=torch.float16)
     float_py = 14.6146
+    int_py = 2
 
     print(f"{(fp16_t * fp32_scalar_t).dtype=}")
     print(f"{(fp32_scalar_t * fp16_t).dtype=}")
     print(f"{(fp16_scalar_t * fp32_scalar_t).dtype=}")
     print(f"{(fp32_scalar_t * fp16_scalar_t).dtype=}")
-    print(f"{(fp16_scalar_t * float_py).dtype=}")
-    print(f"{(float_py * fp16_scalar_t).dtype=}")
+    print(f"{(fp16_t * float_py).dtype=}")
+    print(f"{(float_py * fp16_t).dtype=}")
+    print(f"{(fp16_t / int_py).dtype=}")
 
 
 import torch
