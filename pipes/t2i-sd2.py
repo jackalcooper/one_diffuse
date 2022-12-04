9c87f333ab08b61912b9509e34808dd8e3b81007
jackalcooper <jackalcooper@gmail.com>
2022-12-04 12:09:57 +0800

    add

--- pipes/t2i-sd2.py
+++ pipes/t2i-sd2.py
@@ -1,14 +1,21 @@
 import oneflow as torch
-from diffusers import OneFlowStableDiffusionPipeline as StableDiffusionPipeline, OneFlowEulerDiscreteScheduler as EulerDiscreteScheduler
+from diffusers import (
+    OneFlowStableDiffusionPipeline as StableDiffusionPipeline,
+    OneFlowEulerDiscreteScheduler as EulerDiscreteScheduler,
+)
+
 model_id = "stabilityai/stable-diffusion-2"
 
 # Use the Euler scheduler here instead
 scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16
+)
 pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
 import os
+
 output_dir = "oneflow-sd-output"
 os.makedirs(output_dir, exist_ok=True)
 os.environ["ONEFLOW_MLIR_ENABLE_TIMING"] = "1"
@@ -18,7 +25,8 @@ os.environ["ONEFLOW_MLIR_GROUP_MATMUL"] = "1"
 os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
 with torch.autocast("cuda"):
     for j in range(1000):
-        images = pipe(prompt, height=768, width=768).images
+        # images = pipe(prompt, height=768, width=768).images
+        images = pipe(prompt, height=512, width=512).images
         for i, image in enumerate(images):
             prompt = prompt.strip().replace("\n", " ")
             dst = os.path.join(output_dir, f"{prompt[:100]}-{j}-{i}.png")
