248c97f2a0801e84b7c4011ebaea431254c1bd9d
jackalcooper <jackalcooper@gmail.com>
2022-12-04 14:53:01 +0800

    refine

---
+++ scripts/wash_mlir.py
@@ -0,0 +1,28 @@
+import re
+
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', type=str, required=True)
+args = parser.parse_args()
+
+if not os.isatty(sys.stdin.fileno()):
+    txt = sys.stdin.readlines()
+    txt = "".join(txt)
+else:
+    print("Skip, so it doesn't hang")
+    exit(1)
+
+
+# %[a-zA-Z_0-9#]+
+# output_lbns = output_lbns
+# scope_symbol_id = scope_symbol_id
+# op_name = "OP_NAME"
+txt = re.sub("%[a-zA-Z_0-9#]+", "%VALUE", txt)
+txt = re.sub("output_lbns = \[.+?\]", "OUTPUT_LBNS", txt)
+txt = re.sub("op_name = \".+?\"", "OP_NAME", txt)
+f = open(args.output, "w")
+f.write(txt)
+f.close()
