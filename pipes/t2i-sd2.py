a1d814be30828d2414a11055211bf4d74ff954c8
jackalcooper <jackalcooper@gmail.com>
2022-12-13 15:00:38 +0800

    add

--- pipes/t2i-sd2.py
+++ pipes/t2i-sd2.py
@@ -1,3 +1,22 @@
+import os
+
+os.environ["ONEFLOW_MLIR_CSE"] = "1"
+os.environ["ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION"] = "1"
+os.environ["ONEFLOW_MLIR_ENABLE_ROUND_TRIP"] = "1"
+os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
+os.environ["ONEFLOW_MLIR_GROUP_MATMUL"] = "1"
+os.environ["ONEFLOW_MLIR_PREFER_NHWC"] = "1"
+
+os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_CONV_BIAS"] = "1"
+os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_LINEAR"] = "1"
+
+os.environ["ONEFLOW_KERENL_CONV_ENABLE_CUTLASS_IMPL"] = "1"
+os.environ["ONEFLOW_KERENL_FMHA_ENABLE_TRT_FLASH_ATTN_IMPL"] = "1"
+os.environ["ONEFLOW_KERNEL_GLU_ENABLE_DUAL_GEMM_IMPL"] = "1"
+
+os.environ["ONEFLOW_CONV_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
+os.environ["ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
+
 import oneflow as torch
 from diffusers import (
     OneFlowStableDiffusionPipeline as StableDiffusionPipeline,
@@ -15,27 +34,20 @@ pipe = StableDiffusionPipeline.from_pretrained(
 pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
-import os
+
 
 output_dir = "oneflow-sd-output"
 os.makedirs(output_dir, exist_ok=True)
-os.environ["ONEFLOW_MLIR_ENABLE_TIMING"] = "1"
-os.environ["ONEFLOW_MLIR_PRINT_STATS"] = "1"
-os.environ["ONEFLOW_MLIR_CSE"] = "1"
-os.environ["ONEFLOW_MLIR_GROUP_MATMUL"] = "1"
-os.environ["ONEFLOW_MLIR_CSE"] = "1"
-os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
-os.environ["ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
-os.environ["ONEFLOW_KERENL_ENABLE_TRT_FLASH_ATTN_IMPL"] = "1"
-os.environ["ONEFLOW_NNGRAPH_ENABLE_PROGRESS_BAR"] = "1"
 
+os.environ["ONEFLOW_MLIR_PRINT_STATS"] = "1"
+os.environ["ONEFLOW_NNGRAPH_ENABLE_PROGRESS_BAR"] = "1"
 with torch.autocast("cuda"):
     for j in range(1000):
         pipe.set_unet_graphs_cache_size(8)
         width = random.choice([128, 256, 512, 768])
         height = random.choice([128, 256, 512, 768])
-        width =768+128*random.choice([0,1,2])
-        height =768+128*random.choice([0,1,2])
+        width = 768 + 128 * random.choice([0, 1, 2])
+        height = 768 + 128 * random.choice([0, 1, 2])
         images = pipe(prompt, height=height, width=width).images
         # images = pipe(prompt, height=768, width=768).images
         # images = pipe(prompt, height=512, width=512).images
